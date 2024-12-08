3ab3db5aa1c54bfaf24e3ecdfd2ff37ce960cb9e
Laura Espejo Alvarado <94998786+lauraE090@users.noreply.github.com>
2024-12-08 00:22:51 -0600

    Incorporación de imágenes y actualización de la configuración de tailwind
    - Creación de estilos en input.css
    - Pruebas terminadas de login y registro

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,40 @@
 module.exports = {
   content: ["./**/*.{html,js,php}"],
   theme: {
-    fontFamily: {
-      'roboto': ['Roboto']
+    extend: {
+      colors: {
+        H_519d71: '#519d71',
+        H_DFE3DE: '#DFE3DE',
+        H_393737: '#393737',
+        H_6FCF97: '#6FCF97',
+        H_CCE8D8: '#CCE8D8',
+        H_A7E08E: '#A7E08E',
+        H_38813B: '#38813B',
+        H_4B4848: '#4B4848',
+        H_95C8AA: '#95C8AA',
+        H_618762: '#618762',
+        H_7DB092: '#7DB092',
+        H_DAE5E0: '#DAE5E0',
+        H_E7EBE9: '#E7EBE9',
+        H_ECECEC: '#ECECEC',
+        H_F5F5F5: '#F5F5F5',
+        H_EDF7A3: '#EDF7A3',
+        H_B8F7A3: '#B8F7A3',
+        H_B3B3B3: '#B3B3B3',
+        H_455B3C: '#455B3C',
+        H_E08E8E: '#E08E8E',
+        H_5B3C3C: '#5B3C3C',
+        H_C3CFC9: '#C3CFC9',
+        H_EBF3F0: '#EBF3F0',
+        H_F2F5F0: '#F2F5F0',
+        H_E5E9E4: '#E5E9E4',
+        H_F7F4F4: '#F7F4F4',
+
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
