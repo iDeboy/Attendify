04c94353a6626820225b9c8789d86e1cc17f6023
KMUNIVE <cabalkitzia@gmail.com>
2024-12-08 16:05:04 -0600

    Se agregó contenido Login

--- Pages/loginPage.php
+++ Pages/loginPage.php
@@ -1,23 +1,74 @@
-<div class="login-container">
-    <div class="image-section">
-        <img src="/pagina/img/login1.jpg" alt="Classroom Image">
+<div class="contenedor-principal">
+
+<div class="contenedor-login">
+
+  <div class="elementos-login">
+
+    <div class="elementos-izq">
+
+      <img class="img-class" src="../assets/img/recurso-login.svg" alt="class">
+
     </div>
-    <div class="login-section">
-        <div class="header">
-            <h1 class="header-title">Attendify</h1>
-        </div>
-        <h2>Acceso</h2>
-        <form action="/login" method="POST">
-            <div class="input-group">
-                <label for="email">Correo Electrónico</label>
-                <input type="email" id="email" name="email" placeholder="Correo Electrónico" required>
-            </div>
-            <div class="input-group">
-                <label for="password">Contraseña</label>
-                <input type="password" id="password" name="password" placeholder="Contraseña" required>
-            </div>
-            <button type="submit">Iniciar Sesión</button>
-            <p>¿No tienes una cuenta? <a href="/registro">Regístrate</a></p>
+
+    <div class="elementos-der">
+
+      <div class="tituloApp">
+
+        <p class="nombreApp">Attendify</p>
+        <div class="cuadroAdorn"></div>
+
+      </div>
+
+      <!-- vista login | hidden lo mantiene oculto -->
+      <div class="login hidden">
+
+        <h1>Acceso</h1>
+        <div class="w-[60px] h-[2px] bg-H_38813B mt-1"></div>
+
+        <form action="" method="post">
+
+          <div class="contenedor_texto mt-20 border-b-2 border-gray-400">
+
+            <img class="icon" src="../assets/img/email.svg" alt="email">
+
+            <!-- correo electronico -->
+            <input type="text"
+              placeholder="CORREO ELECTRONICO"
+              class="caja_login">
+
+          </div>
+
+          <div class="contenedor_texto mt-8 border-b-2 border-gray-400">
+
+            <img class="icon" src="../assets/img/psswd.svg" alt="password">
+
+            <!-- contraseña -->
+            <input type="password"
+              placeholder="CONTRASEÑA"
+              class="caja_login">
+
+          </div>
+
+          <div class="mt-1 flex items-center justify-end space-x-2 w-full">
+            <input type="checkbox" class="form-checkbox text-blue-500">
+            <label class="text-sm text-H_393737 texto">Mostrar contraseña</label>
+          </div>
+
+          <!-- boton de iniciar sesión -->
+          <button type="submit" class="boton mt-20">INICIAR SESIÓN</button>
+
+          <p class="texto mt-2 text-[16px] text-H_393737">¿No tienes una cuenta?
+            <!-- vinvulo para el registro -->
+            <a href="#" class="text-H_A7E08E hover:text-H_618762 texto">Regístrate</a>
+          </p>
+
         </form>
+
+      </div>
+
     </div>
+
+  </div>
+
+</div>
 </div>
