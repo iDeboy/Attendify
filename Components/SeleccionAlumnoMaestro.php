c0875d39817a4704c37bce5fe0f516f0b996d0be
Laura Espejo Alvarado <94998786+lauraE090@users.noreply.github.com>
2024-12-08 21:31:42 -0600

    Arreglos de paginas y estilos
    - Modificación de LoginPage y cambio en su nombre a L mayúscula.
    - Modificación de RegistroPage
    - Se agregaron 2 componentes: Checkbox y SeleccionAlumnoMaestro.

---
+++ Components/SeleccionAlumnoMaestro.php
@@ -0,0 +1,10 @@
+<div class="radio-inputs">
+  <label class="radio">
+    <input type="radio" name="radio" checked="">
+    <span class="name">ALUMNO</span>
+  </label>
+  <label class="radio">
+    <input type="radio" name="radio">
+    <span class="name">DOCENTE</span>
+  </label>
+</div>
--- Pages/loginPage.php
+++ Pages/loginPage.php
@@ -1,74 +1,72 @@
 <div class="contenedor-principal">
 
-<div class="contenedor-login">
+  <div class="contenedor-login">
 
-  <div class="elementos-login">
+    <div class="elementos-login">
 
-    <div class="elementos-izq">
+      <div class="elementos-izq">
 
-      <img class="img-class" src="../assets/img/recurso-login.svg" alt="class">
-
-    </div>
-
-    <div class="elementos-der">
-
-      <div class="tituloApp">
-
-        <p class="nombreApp">Attendify</p>
-        <div class="cuadroAdorn"></div>
+        <img class="img-class" src="../assets/img/recurso-login.svg" alt="class">
 
       </div>
 
-      <!-- vista login | hidden lo mantiene oculto -->
-      <div class="login hidden">
+      <div class="elementos-der">
 
-        <h1>Acceso</h1>
-        <div class="w-[60px] h-[2px] bg-H_38813B mt-1"></div>
+        <div class="tituloApp">
 
-        <form action="" method="post">
+          <p class="nombreApp">Attendify</p>
+          <div class="cuadroAdorn"></div>
 
-          <div class="contenedor_texto mt-20 border-b-2 border-gray-400">
+        </div>
 
-            <img class="icon" src="../assets/img/email.svg" alt="email">
+        <!-- vista login | hidden lo mantiene oculto -->
+        <div class="login">
 
-            <!-- correo electronico -->
-            <input type="text"
-              placeholder="CORREO ELECTRONICO"
-              class="caja_login">
+          <h1>Acceso</h1>
+          <div class="w-[60px] h-[2px] bg-H_38813B mt-1"></div>
 
-          </div>
+          <form action="" method="post">
 
-          <div class="contenedor_texto mt-8 border-b-2 border-gray-400">
+            <div class="contenedor_texto mt-20 border-b-2 border-gray-400">
 
-            <img class="icon" src="../assets/img/psswd.svg" alt="password">
+              <img class="icon" src="../assets/img/email.svg" alt="email">
 
-            <!-- contraseña -->
-            <input type="password"
-              placeholder="CONTRASEÑA"
-              class="caja_login">
+              <!-- ingresar correo electronico -->
+              <input type="text"
+                placeholder="CORREO ELECTRONICO"
+                class="caja_login">
 
-          </div>
+            </div>
 
-          <div class="mt-1 flex items-center justify-end space-x-2 w-full">
-            <input type="checkbox" class="form-checkbox text-blue-500">
-            <label class="text-sm text-H_393737 texto">Mostrar contraseña</label>
-          </div>
+            <div class="contenedor_texto mt-8 border-b-2 border-gray-400">
 
-          <!-- boton de iniciar sesión -->
-          <button type="submit" class="boton mt-20">INICIAR SESIÓN</button>
+              <img class="icon" src="../assets/img/psswd.svg" alt="password">
 
-          <p class="texto mt-2 text-[16px] text-H_393737">¿No tienes una cuenta?
-            <!-- vinvulo para el registro -->
-            <a href="#" class="text-H_A7E08E hover:text-H_618762 texto">Regístrate</a>
-          </p>
+              <!-- ingresar contraseña -->
+              <input type="password"
+                placeholder="CONTRASEÑA"
+                class="caja_login">
 
-        </form>
+            </div>
+
+            <!-- checkbox para mostrar contraseña - hacer que al marcarla se visualice y al desmarcar se vuelva a ocular -->
+            <?php echo render_template("Components/Checkbox.php", ["Texto" => "Mostrar Contraseña"]) ?>
+
+            <!-- boton de iniciar sesión -->
+            <button type="submit" class="boton mt-20">INICIAR SESIÓN</button>
+
+            <p class="texto mt-2 text-[16px] text-H_393737">¿No tienes una cuenta?
+              <!-- vinvulo para mandar al registro -->
+              <a href="#" class="text-H_A7E08E hover:text-H_618762 texto">Regístrate</a>
+            </p>
+
+          </form>
+
+        </div>
 
       </div>
 
     </div>
 
   </div>
-
-</div>
 </div>
