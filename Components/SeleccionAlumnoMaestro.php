f11d3dd8b1aebf4dd85239566cf31182a8866e6e
iDeboy <Theboygamer77@gmail.com>
2024-12-08 23:44:11 -0600

    Añadido 'value' a inputs del componente SeleccionAlumnoMaestro

--- Components/SeleccionAlumnoMaestro.php
+++ Components/SeleccionAlumnoMaestro.php
@@ -1,10 +1,10 @@
 <div class="radio-inputs">
   <label class="radio">
-    <input type="radio" name="radio" checked="">
+    <input type="radio" name="radio" value="Alumno" checked>
     <span class="name">ALUMNO</span>
   </label>
   <label class="radio">
-    <input type="radio" name="radio">
+    <input type="radio" name="radio" value="Docente">
     <span class="name">DOCENTE</span>
   </label>
 </div>
