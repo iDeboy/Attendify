b43dbd2dc96acafda3d77e477498c27dba2fb14e
iDeboy <Theboygamer77@gmail.com>
2024-12-10 23:11:34 -0600

    Añadido DbContext a DI para la conexión con la base de datos, se determina autmatico el servidor

--- program.php
+++ program.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Abstractions\DbContext;
 use Abstractions\HostBuilder;
 use Abstractions\Renderer;
 use Abstractions\Router;
@@ -14,9 +15,16 @@ $builder = HostBuilder::default_builder();
 $builder->services
     ->add_singleton(Renderer::class, fn(): Renderer => new Renderer('App.php'))
     ->add_singleton(Router::class)
+    ->add_transient(DbContext::class, function (): DbContext {
+
+        if (strcmp($_SERVER['SERVER_SOFTWARE'], 'LiteSpeed')  === 0)
+            return new DbContext('auth-db1212.hstgr.io', 'u117281852_w24021001', 'Honorio2401$', 'u117281852_w24021001');
+        else
+            return new DbContext('localhost', 'root', '', 'asistencia');
+    })
     ->add_transient(PruebasController::class)
     ->add_transient(LoginController::class)
     ->add_transient(RegistroController::class)
-    ;
+;
 
 return $builder->build();
