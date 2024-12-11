0e5bd98ec1aa1f85e2fcf5dd4f73d0cde788e723
Laura Espejo Alvarado <94998786+lauraE090@users.noreply.github.com>
2024-12-11 15:28:23 -0600

    Agregado de AlumnoGrupoDispPage.php
    La página agregada corresponde a la muestra de grupos disponible a inscribirse para el alumno.
    - Se agregaron animaciones al archivo de configuración de Tailwind.
    - Ruta: /alumno/grupos-disponibles
    - Layout: Layouts/AlumnoLayout.php

--- Pages/AlumnoPrincipalPage.php
+++ Pages/AlumnoPrincipalPage.php
@@ -1,8 +1,8 @@
-<div class="p-6 max-w-4xl mx-auto bg-H_EBF3F0 rounded-md shadow-md mb-4">
+<div class="p-6 max-w-4xl mx-auto bg-H_EBF3F0 rounded-md shadow-md mb-4 animate-fade-in">
     <h2 class="text-H_393737 font-semibold text-xl">Bienvenid@ {{Alumno.nombre}}{{Alumno.apellidos}}, ¡Hoy es un gran día para aprender!</h2>
 </div>
 
-<div class="p-6 max-w-4xl mx-auto bg-H_EBF3F0 rounded-md shadow-md">
+<div class="p-6 max-w-4xl mx-auto bg-H_EBF3F0 rounded-md shadow-md animate-fade-in">
 
     <!-- Asistencias pendientes -->
     <div class="bg-H_DAE5E0 p-4 rounded-md shadow mb-6">
@@ -13,7 +13,7 @@
         <div class="h-[280px] overflow-y-auto p-1">
             <!-- Si no hay asistencias pendientes no aparecian los grupos de asistencias (como el que se muestra debajo), aparece por cada asistencia pendiente al dia actual -->
             <div class="bg-white p-4 rounded-md shadow-md mb-2 hidden">
-                <h3 class="font-bold text-H_455B3C mb-2">{{Materia.codigoMateria}} | {{Materia.nombreMateria}}</h3>
+                <h3 class="font-bold text-H_455B3C mb-2">{{Grupo.nombreGrupo}} | {{Materia.nombreMateria}}</h3>
                 <p class="text-H_4B4848 mb-2">{{TemasGrupoMateria.tema}}</p>
                 <div class="flex items-center text-H_393737">
                     <span class="mdi mdi-calendar-month text-xl mr-2"></span>
@@ -39,7 +39,7 @@
                 <div class="flex items-center">
                     <span class="mdi mdi-account-group text-xl mr-2 text-H_393737"></span>
                     <!-- Verificar como obtenemos la materia a la que fue enviada la solicitud -->
-                    <span class=" text-H_393737">{{Materia.codigoMateria}} | {{Materia.nombreMateria}}</span>
+                    <span class=" text-H_393737">{{Grupo.nombreGrupo}} | {{Materia.nombreMateria}}</span>
                 </div>
                 <div class="flex items-center">
                     <span class="text-H_393737 mr-2">Estado:</span>
