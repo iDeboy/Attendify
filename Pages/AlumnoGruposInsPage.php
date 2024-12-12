634d36dbca293aeb0d386dd2107dee6ffc1eca9b
iDeboy <Theboygamer77@gmail.com>
2024-12-11 22:01:23 -0600

    Formateo

--- Pages/AlumnoGruposInsPage.php
+++ Pages/AlumnoGruposInsPage.php
@@ -1,37 +1,37 @@
-<div class="p-0 max-w-4xl mx-auto bg-H_EBF3F0 rounded-md shadow-md mb-4 animate-fade-in">
-        <div class="flex items-center justify-center p-5">
-            <div class="w-full rounded-lg p-1">
-                <!-- Buscador - Se podra buscar un grupo por su nombre {{Grupo.nombreGrupo}}, aparecen los grupos en los que el alumno se encuentra inscrito. -->
-                <div class="flex">
-                    <div class="flex w-10 items-center justify-center rounded-tl-lg rounded-bl-lg border-r border-gray-200 bg-white p-1">
-                        <span class="mdi mdi-magnify text-xl text-H_393737 pointer-events-none"></span>
-                    </div>
-                    <!-- Aqui se encuentra la caja de texto del buscador -->
-                    <input type="text"
-                        class="w-full text-H_393737 bg-white pl-2 text-base outline-0 relative tooltip-trigger"
-                        placeholder="Buscar . . ."
-                        id=""
-                        title="Busca la materia por su codigo o nombre">
-                    <!-- Boton de buscar -->
-                    <input type="button" value="Buscar" class=" bg-[#779688] p-2 rounded-tr-lg rounded-br-lg text-white font-semibold hover:bg-[#DAE5E0] hover:text-H_618762 transition-colors">
+<div class="max-w-4xl p-0 mx-auto mb-4 rounded-md shadow-md bg-H_EBF3F0 animate-fade-in">
+    <div class="flex items-center justify-center p-5">
+        <div class="w-full p-1 rounded-lg">
+            <!-- Buscador - Se podra buscar un grupo por su nombre {{Grupo.nombreGrupo}}, aparecen los grupos en los que el alumno se encuentra inscrito. -->
+            <div class="flex">
+                <div class="flex items-center justify-center w-10 p-1 bg-white border-r border-gray-200 rounded-tl-lg rounded-bl-lg">
+                    <span class="text-xl pointer-events-none mdi mdi-magnify text-H_393737"></span>
                 </div>
+                <!-- Aqui se encuentra la caja de texto del buscador -->
+                <input type="text"
+                    class="relative w-full pl-2 text-base bg-white text-H_393737 outline-0 tooltip-trigger"
+                    placeholder="Buscar . . ."
+                    id=""
+                    title="Busca la materia por su codigo o nombre">
+                <!-- Boton de buscar -->
+                <input type="button" value="Buscar" class=" bg-[#779688] p-2 rounded-tr-lg rounded-br-lg text-white font-semibold hover:bg-[#DAE5E0] hover:text-H_618762 transition-colors">
             </div>
         </div>
+    </div>
 </div>
 
-<div class="p-6 max-w-4xl mx-auto bg-H_EBF3F0 rounded-md shadow-md animate-fade-in">
+<div class="max-w-4xl p-6 mx-auto rounded-md shadow-md bg-H_EBF3F0 animate-fade-in">
 
-    <div class="bg-H_DAE5E0 p-4 rounded-md shadow mb-6">
+    <div class="p-4 mb-6 rounded-md shadow bg-H_DAE5E0">
 
-        <h2 class="text-H_393737 font-semibold text-xl mb-4">Mis Grupos</h2>
+        <h2 class="mb-4 text-xl font-semibold text-H_393737">Mis Grupos</h2>
 
         <div class="h-[280px] overflow-y-auto p-1">
             <!-- Muestra de como se representa el grupo -->
-            <div class="bg-white p-4 rounded-md shadow-md mb-2">
-                <h3 class="font-bold text-H_455B3C mb-2">{{Grupo.nombreGrupo}} | {{Materia.nombreMateria}}</h3>
-                <div class="bg-white p-1 flex justify-between items-center">
+            <div class="p-4 mb-2 bg-white rounded-md shadow-md">
+                <h3 class="mb-2 font-bold text-H_455B3C">{{Grupo.nombreGrupo}} | {{Materia.nombreMateria}}</h3>
+                <div class="flex items-center justify-between p-1 bg-white">
                     <div class="flex items-center">
-                        <span class="mdi mdi-town-hall text-xl mr-2 text-H_393737"></span>
+                        <span class="mr-2 text-xl mdi mdi-town-hall text-H_393737"></span>
                         <span class=" text-H_393737">Impartido por: {{Profesor.nombre}}</span>
                     </div>
                     <div class="flex items-center">
@@ -41,9 +41,9 @@
                             type="button">
                             <div
                                 class="bg-[#779688] h-8 w-1/4 flex items-center justify-center absolute left-1 top-[4px] group-hover:w-[168px] z-10 duration-500">
-                                <span class="mdi mdi-arrow-right-thick text-xl text-white"></span>
+                                <span class="text-xl text-white mdi mdi-arrow-right-thick"></span>
                             </div>
-                            <p class="translate-x-2 ml-10">Ingresar</p>
+                            <p class="ml-10 translate-x-2">Ingresar</p>
                         </button>
                     </div>
                 </div>
