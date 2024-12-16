dd34ad702117cbc97914b0052622769878e429ec
Honorio Acosta Ruiz <116129311+iDeboy@users.noreply.github.com>
2024-12-15 21:02:21 -0600

    Preparando Acerca de

--- Layouts/ProfesorLayout.php
+++ Layouts/ProfesorLayout.php
@@ -31,6 +31,11 @@
                         Grupos
                     </a>
 
+                    <a href="acerca-de" class="block transicion text-[18px]">
+                        <span class="mdi mdi-information mr-2 select-none text-[20px]"></span>
+                        Acerca de
+                    </a>
+
                     <!-- Regresar al apartado de iniciar sesión -->
                     <form action="logout" method="post">
                         <button type="submit" class="block transicion text-[18px] w-full text-start">
